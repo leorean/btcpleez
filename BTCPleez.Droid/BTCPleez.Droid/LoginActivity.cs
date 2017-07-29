b133e796baeb796e8af5314d31ab9068beeef42f
Peter S <peter@anyline.io>
2017-07-29 15:59:11 +0200

    login mask

---
+++ BTCPleez.Droid/BTCPleez.Droid/LoginActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace BTCPleez.Droid
+{
+    [Activity(Label = "BTCPleez", MainLauncher = true, Icon = "@drawable/icon")]
+    public class LoginActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Window.RequestFeature(WindowFeatures.ActionBar);
+            ActionBar.Hide();
+
+            // Set our view from the layout resource
+            SetContentView (Resource.Layout.Login);
+
+            var btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+
+            btnLogin.Click -= BtnLogin_Click;
+            btnLogin.Click += BtnLogin_Click;
+        }
+
+        private void BtnLogin_Click(object sender, System.EventArgs e)
+        {
+            Toast.MakeText(this, "Test", ToastLength.Short);
+        }
+    }
+}
+
--- BTCPleez.Droid/BTCPleez.Droid/Resources/Resource.Designer.cs
+++ BTCPleez.Droid/BTCPleez.Droid/Resources/Resource.Designer.cs
@@ -47,6 +47,9 @@ namespace BTCPleez.Droid
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int logo = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -57,11 +60,36 @@ namespace BTCPleez.Droid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050003
+			public const int btnLogin = 2131034115;
+			
+			// aapt resource value: 0x7f050000
+			public const int imgLogo = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtPassword = 2131034114;
+			
+			// aapt resource value: 0x7f050001
+			public const int txtUser = 2131034113;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
 			
 			static Layout()
 			{
